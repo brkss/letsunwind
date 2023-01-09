eff2598bac43c33701f0dd8cf341f84f11c5656c
Brahim Berkasse <berkassebrahim@gmail.com>
2023-01-09 21:43:53 +0100

    🪖 auth btns !

--- app/src/screens/Intro.tsx
+++ app/src/screens/Intro.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, View, StyleSheet, Pressable } from 'react-native';
 import Animated, {useAnimatedStyle, useSharedValue, withDelay,  withTiming } from 'react-native-reanimated';
 
 const intros = [
-	"students are exposed to a lot of pressure and stress, which negatively impacts their mental health",
-	"In the past decade, the number of students treated for mental disorders has been on the rise",
+	"1- students are exposed to a lot of pressure and stress, which negatively impacts their mental health",
+	"2- In the past decade, the number of students treated for mental disorders has been on the rise",
 	/*
 	"a survey conducted by the US National Alliance on Mental Illness resulted that 64% of students drop out of college because of mental health problems.",
 	"The prevalence of depression among college students was 41% in 2021.",
@@ -14,14 +14,18 @@ const intros = [
 	"Suicide rates of college students have tripled since the 1950s.",
 	"In the 2020–2021 school year, 33% of students receiving mental health services considered suicide.",
 	"Coping with new challenges and excessive academic demands often result in burnout, leading to stress and anxiety.",
-*/
+	*/
 ]
 
-export const Intro : React.FC = () => {
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
+
+export const Intro : React.FC<any> = ({navigation}) => {
 
 	const opacity = useSharedValue<number>(0);
 	const [curr, setCurr] = React.useState(0);
-
+	let started = false
+	const regBtnOpacity = useSharedValue<number>(0);
+	const logBtnOpacity = useSharedValue<number>(0);
 
 	// fading animation 
 	const display = () => {
@@ -30,19 +34,33 @@ export const Intro : React.FC = () => {
 	}
 
 	React.useEffect(() => {
-		display();
-		const interval = setInterval(() => {
-			if(curr < intros.length - 1){
-				setCurr(curr + 1);	
-			}
-			else{
-				console.log("clear interval")
-				clearInterval(interval)
-			}
+		let interval: NodeJS.Timer;
+		if(!started){
+			started = true;
 			display();
-		}, 7700)
+			interval = setInterval(() => {
+				console.log("running !");
+				if(curr <= intros.length - 1){
+					setCurr(curr + 1);	
+				}
+				else{
+					console.log("clear interval")
+					regBtnOpacity.value = withTiming(1, {duration: 800})
+					//--> navigation.navigate('HomeNav')
+					clearInterval(interval)
+				}
+				display();
+			}, 7700)
+		}
+		
 		return () => clearInterval(interval)
 	}, [curr])
+
+	const regBtnStyle = useAnimatedStyle(() => {
+		return {
+			opacity: regBtnOpacity.value
+		}
+	})
 
 	const introTxtStyle = useAnimatedStyle(() => {
 		return {
@@ -54,6 +72,12 @@ export const Intro : React.FC = () => {
 		<SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
 			<View style={styles.container}>
 				<Animated.Text style={[styles.txt, introTxtStyle]}>{intros[curr]}</Animated.Text>
+				<AnimatedPressable style={[{backgroundColor: 'white'}, regBtnStyle, styles.btn]}>
+					<Text style={[styles.btnTxt, {color: 'black'}]}>Create Account</Text>
+				</AnimatedPressable>
+				<AnimatedPressable style={[{}, regBtnStyle, styles.btn]}>
+					<Text style={styles.btnTxt}>Login</Text>
+				</AnimatedPressable>
 			</View>							
 		</SafeAreaView>
 	)
@@ -70,7 +94,19 @@ const styles = StyleSheet.create({
 		//
 	},
 	btn: {
-		//
+		marginBottom: 15,
+		padding: 20,
+		width: '100%',
+		borderColor: 'white',
+		borderWidth: 2,
+		borderRadius: 17
+	},
+	btnTxt: {
+		fontSize: 21,
+		color: 'white',
+		fontWeight: 'bold',
+		textAlign: 'center',
+		fontFamily: 'cooper'
 	},
 	txt: {
 		color: 'white',
