b58bc5a29b910283dc899da0e66f0192560cf8bc
Brahim Berkasse <berkassebrahim@gmail.com>
2023-01-14 01:48:20 +0100

    🖇️  user quesries !

--- server/db/sqlc/user_test.go
+++ server/db/sqlc/user_test.go
@@ -2,14 +2,16 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/brkss/go-auth/utils"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
 )
 
 
-func CreateUser(t *testing.T) User {
+func CreateRandomUser(t *testing.T) User {
 
 	arg := CreateUserParams{
 		ID: uuid.New().String(),	
@@ -18,6 +20,38 @@ func CreateUser(t *testing.T) User {
 		Age: 22,
 	}
 
-	user, err := testQueries.createUser(context.Background(), arg)
+	user, err := testQueries.CreateUser(context.Background(), arg)
 
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, user.Age, arg.Age)
+	require.Equal(t, user.Email, arg.Email)
+	require.Equal(t, user.Name, arg.Name)
+	require.Equal(t, user.ID, arg.ID)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T){
+	CreateRandomUser(t)
+}
+
+func TestGetUserByEmail(t *testing.T){
+	seed := CreateRandomUser(t)
+	
+
+	user, err := testQueries.GetUserByEmail(context.Background(), seed.Email)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, user.Age, seed.Age)
+	require.Equal(t, user.Email, seed.Email)
+	require.Equal(t, user.Name, seed.Name)
+	require.Equal(t, user.ID, seed.ID)
+
+	
+	_, err = testQueries.GetUserByEmail(context.Background(), utils.RandomEmail())
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
 }
